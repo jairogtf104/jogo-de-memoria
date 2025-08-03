991fe0becfa713a9f8053d88f1d30c20a0037717
jairo <jairofilho99@hotmail.com>
2025-08-03 15:22:42 -0300

    iniciandoAinteracaoComJava

---
+++ driven.js
@@ -0,0 +1,19 @@
+function numeroDeCartas() {
+
+    let numeroDasCartas = Number(prompt("qual o numero de cartas?"));
+
+    while(numeroDasCartas % 2 !== 0){
+
+        numeroDasCartas = Number(prompt("qual o numero de cartas?"));
+
+        if(numeroDasCartas % 2 ===0 ){
+
+            console.log("cartas");
+        }
+
+    }
+
+
+}
+
+window.onload = numeroDeCartas;
