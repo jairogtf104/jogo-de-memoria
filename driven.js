966d4d7a8af72c35e1beb3f39a06963092e16e3e
jairo <jairofilho99@hotmail.com>
2025-08-15 05:40:12 -0300

    abcdef

--- driven.js
+++ driven.js
@@ -4,9 +4,12 @@ let carta1 =" ";
 
 let carta2 =" ";
 
+let paraClique = "";
+
+
 const listaDeImagens = [
     "assets/bobrossparrot.gif", 
-    "assets/explodyparrot.gif",
+    "assets/bobrossparrot.gif",
     "assets/fiestaparrot.gif", 
     "assets/metalparrot.gif",
     "assets/revertitparrot.gif",
@@ -42,7 +45,7 @@ function criaCarta() {
                 <img src="assets/back.png">
             </div>
             <div class="back-face face">
-                 <img src=${listaDeImagens[contador * 2]}>
+                 <img src=${listaDeImagens[contador]}>
             </div>
         </div>`;
 
@@ -51,7 +54,6 @@ function criaCarta() {
         contador++;
 
     }
-
 
 
 }
@@ -66,17 +68,27 @@ function clickCarta(){
 
             card.classList.add('flip');
 
-            this.classList.add("flip");
+            let cartaClicada =  this.classList.add("flip");
 
             carta1 = document.querySelector(".back-face img").src;
 
             carta2 = document.querySelector(".back-face img").src;
 
-            if(carta1 !== carta2){
+            if(carta1 === " "){
 
-                carta1 =" ";
+                carta1 = cartaClicada;
 
-                carta2 =" ";
+            }
+
+            if(carta2 === " "){
+
+                carta2 = cartaClicada;
+
+            }
+
+            if (carta1 !== carta2){
+
+                cartaClicada =  this.classList.remove("flip");
 
             }
             
